06577b04ead59031603e179d5df6c58885a5d2e5
chiro <sachinsinghroxx@gmail.com>
2020-12-03 12:55:09 +0530

    asserted 1

--- sign_language_app.py
+++ sign_language_app.py
@@ -101,7 +101,7 @@ def runApp(batchSize, threadnum, image_dir,model):
     out_q = [None] * runTotal
     g = xir.graph.Graph.deserialize(pathlib.Path(model))
     subgraph = get_subgraph(g)
-    assert len(subgraph) == 100
+    assert len(subgraph) == 1
     all_dpu_runners = []
     for i in range(threads):
         all_dpu_runners.append(runner.Runner(subgraphs[0],"run"))
