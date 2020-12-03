454b0507612bf06b5e3e1d8621112a1c68fa3d60
chiro <sachinsinghroxx@gmail.com>
2020-12-03 13:01:03 +0530

    removed grey

--- sign_language_app.py
+++ sign_language_app.py
@@ -111,7 +111,7 @@ def runApp(batchSize, threads, image_dir,model):
     img = []
     for i in range(len(listImage)):
         image = cv2.imread(os.path.join(image_dir,listImage[i]), cv2.IMREAD_GRAYSCALE)
-        image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        # image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
         image = cv2.resize(image,200,200)
         image = image.reshape(-1,200,200,1).astype('float32')
         image = image/255.0
